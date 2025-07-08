262b4673aab58a28e42a12926e68d77e63940503
Henry <henrybassey002@gmail.com>
2025-07-08 01:44:10 +0100

    feat: implement dynamic order status update and cancel actions.

--- src/Pages/MenuItem/MenuItemList.tsx
+++ src/Pages/MenuItem/MenuItemList.tsx
@@ -16,7 +16,7 @@ function MenuItemList() {
           </div>
           <div className="p-2">
             <div className="row border">
-              <div className="col-2">Image</div>
+              <div className="col-1">Image</div>
               <div className="col-1">ID</div>
               <div className="col-2">Name</div>
               <div className="col-2">Category</div>
@@ -28,18 +28,18 @@ function MenuItemList() {
             {data.result.map((menuItem: menuItemModel) => {
               return (
                 <div className="row border" key={menuItem.id}>
-                  <div className="col-2">
+                  <div className="col-1">
                     <img
-                      src="https://via.placeholder.com/150"
+                      src={menuItem.image}
                       alt="no content"
                       style={{ width: "100%", maxWidth: "120px" }}
                     />
                   </div>
-                  <div className="col-1">.id</div>
-                  <div className="col-2">.name</div>
-                  <div className="col-2">.category</div>
-                  <div className="col-1">.price</div>
-                  <div className="col-2">.specialTag</div>
+                  <div className="col-1">{menuItem.id}</div>
+                  <div className="col-2">{menuItem.name}</div>
+                  <div className="col-2">{menuItem.category}</div>
+                  <div className="col-1">${menuItem.price}</div>
+                  <div className="col-2">{menuItem.specialTag}</div>
                   <div className="col-2">
                     <button className="btn btn-success">
                       <i className="bi bi-pencil-fill"></i>
@@ -57,5 +57,4 @@ function MenuItemList() {
     </>
   );
 }
-
 export default MenuItemList;
---
+++ src/Pages/MenuItem/MenuItemUpsert.tsx
@@ -0,0 +1,9 @@
+function MenuItemUpsert() {
+  return (
+    <div>
+      <h2>Menu Item Upsert</h2>
+    </div>
+  );
+}
+
+export default MenuItemUpsert;
